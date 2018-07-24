102530114692abbce3397e694e32af0c48510e76
SondreB <pixelworld@hotmail.com>
2018-07-24 23:33:35 +0200

    Add support for mobile #7
    - Not complete yet, but adds some basic testing that connects to the new SimpleWallet on the Full Node.
    - More work require a fork of bitcore-lib that supports Stratis/City Chain.

--- src/app/components/load/load.component.ts
+++ src/app/components/load/load.component.ts
@@ -2,6 +2,11 @@ import { Component, ViewEncapsulation, ChangeDetectionStrategy, HostBinding } fr
 import { AuthenticationService } from '../../services/authentication.service';
 import { Router } from '@angular/router';
 import { ApplicationStateService } from '../../services/application-state.service';
+import { HubConnectionBuilder } from '@aspnet/signalr';
+import * as signalR from '@aspnet/signalr';
+import { HDNode, Transaction } from 'bitcoinjs-lib';
+const bip39 = require('bip39');
+const bitcoin = require('bitcoinjs-lib');
 
 export interface Mode {
     name: string;
@@ -20,6 +25,7 @@ export class LoadComponent {
     hasWallet = false;
     modes: Mode[] = [];
     remember: boolean;
+    connection: signalR.HubConnection;
 
     constructor(private authService: AuthenticationService, private router: Router, private appState: ApplicationStateService) {
 
@@ -27,8 +33,8 @@ export class LoadComponent {
             { id: 'simple', name: 'Mobile' },
             { id: 'light', name: 'Light' },
             { id: 'full', name: 'Full' },
-            { id: 'pos', name: 'Point-of-Sale (POS)' },
-            { id: 'readonly', name: 'Read-only' }
+            //{ id: 'pos', name: 'Point-of-Sale (POS)' },
+            //{ id: 'readonly', name: 'Read-only' }
         ];
 
         // If user has choosen to remember mode, we'll redirect directly to login.
@@ -45,5 +51,49 @@ export class LoadComponent {
         this.appState.mode = this.selectedMode.id;
 
         this.router.navigateByUrl('/login');
+    }
+
+    simpleWalletConnect() {
+
+        // Meet the new stack for real-time web communication: ASP.NET Core SignalR
+        // Learn more: https://www.youtube.com/watch?v=Lws0zOaseIM
+
+        this.connection = new signalR.HubConnectionBuilder()
+            .withUrl('http://localhost:8081/wallet')
+            .build();
+
+        this.connection.on('ReceiveMessage', (user, message) => {
+            console.log(user);
+            console.log(message);
+        });
+
+        this.connection.on('txs', (transactions) => {
+
+            console.log(transactions);
+
+            // TODO: Update a bitcore-lib fork to add support for Stratis/City Chain.
+            // var tx1 = transactions[0];
+            // var tx = bitcoin.Transaction.fromHex(tx1.value.hex);
+        });
+
+        const self = this;
+
+        // Transport fallback functionality is now built into start.
+        this.connection.start()
+            .then(function () {
+                console.log('connection started');
+                self.connection.invoke('CreateWallet', 'cityhub-mobile-0.0.1', '2018-01-01', ['SMsZGWkF9zR5mjxWTYsDq9pU5ZHdUQw1jJ']).catch(err => console.error(err.toString()));
+            })
+            .catch(error => {
+                console.error(error.message);
+            });
+    }
+
+    simpleWalletWatch() {
+        this.connection.invoke('Watch', 'SMsZGWkF9zR5mjxWTYsDq9pU5ZHdUQw1jJ').catch(err => console.error(err.toString()));
+    }
+
+    simpleWalletBalance() {
+        this.connection.invoke('Balance', 'SMsZGWkF9zR5mjxWTYsDq9pU5ZHdUQw1jJ').catch(err => console.error(err.toString()));
     }
 }
