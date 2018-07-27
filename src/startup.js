607e6c3f6f4671d17d9faf6ba2ecf2a33ac9e6cc
SondreB <pixelworld@hotmail.com>
2018-07-27 13:50:51 +0200

    Add City Hub logo in chrome (#15)
    - Add the City Hub logo in the chrome window, removing the temporary City Chain logo.

--- src/startup.js
+++ src/startup.js
@@ -9,7 +9,10 @@
   const coin = getParam('coin') || 'city';
 
   console.log('Selected coin: ' + coin);
-  document.getElementById('title-bar-logo').setAttribute('src', '/assets/' + coin + '/logo-full-dark.png');
+
+  // We don't really want to change the whole branding, City Hub will support multiple coins. But if anyone
+  // want to fork and release a more generic app with multi-coin support, they could change top-logo this way.
+  // document.getElementById('title-bar-logo').setAttribute('src', '/assets/' + coin + '/logo-full-dark.png');
 
   var userAgent = navigator.userAgent.toLowerCase();
   if (userAgent.indexOf('electron') > -1) {
