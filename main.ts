5571c52e1254bf5a9b8d5db9a2de683445d82558
SondreB <sondre@outlook.com>
2020-08-16 13:47:04 +0200

    Minor edit to trigger build

--- main.ts
+++ main.ts
@@ -299,8 +299,6 @@ function createWindow() {
         shell.openExternal(linkUrl);
     });
 
-
-
     if (serve) {
         require('electron-reload')(__dirname, {
             electron: require(`${__dirname}/node_modules/electron`)
