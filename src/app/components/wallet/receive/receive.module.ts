0dbe6221927e0583791caa9f2bf69b4893aafce3
SondreB <sondre@outlook.com>
2022-04-01 15:22:14 +0200

    Remove old module import

--- src/app/components/wallet/receive/receive.module.ts
+++ src/app/components/wallet/receive/receive.module.ts
@@ -3,13 +3,11 @@ import { ReceiveComponent } from './receive.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../../../material.module';
-import { NgxQRCodeModule } from '@techiediaries/ngx-qrcode';
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule,
-        NgxQRCodeModule,
         MaterialModule,
     ],
     declarations: [
